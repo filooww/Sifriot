c82bb795bcf30bbb92bb4f23001631e651e1717d
ElvinQad <qadirov.elvin.qe@gmail.com>
2025-12-27 14:01:59 +0400

    chore: Remove legacy HTDocs directory and associated files.

--- HTDocs_legacy/index.php
+++
@@ -1,8 +0,0 @@
-<html>
-<body>
-<h1>It works!</h1>
-and so on ...
-</body>
-</html>
-
-<?php echo phpinfo(); ?>
--- HTDocs_legacy/s/SystemDataBaseCopies/sys_tables18052024/db_list.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `db_list` (`db_id`, `db_name`, `db_coding`, `db_comment`, `db_err`) VALUES
-(-2, 'kk', '', '', 0),
-(1, 'literature', 'utf8', 'literature', 0),
-(2, '', '', '', 0),
-(3, 'phys_math_contents', 'utf8', 'qu', 0);
-
